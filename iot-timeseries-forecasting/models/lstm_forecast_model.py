da1fe099fb89f39863e961b2578e037785d519a8
beratkaya61 <kayaberat.burak@gmail.com>
2025-07-17 11:21:27 +0300

    optimize hyper parameters of lstm model and save outputs

--- iot-timeseries-forecasting/models/lstm_forecast_model.py
+++ iot-timeseries-forecasting/models/lstm_forecast_model.py
@@ -1,10 +1,23 @@
 # 🧠 Step 4: Define LSTM Model
 import torch.nn as nn
 
+# class LSTMForecast(nn.Module):
+#     def __init__(self, hidden_size=64):
+#         super().__init__()
+#         self.lstm = nn.LSTM(input_size=1, hidden_size=hidden_size, batch_first=True)
+#         self.fc = nn.Linear(hidden_size, 1)
+
+#     def forward(self, x):
+#         output, _ = self.lstm(x)
+#         return self.fc(output[:, -1, :])
+
+
 class LSTMForecast(nn.Module):
-    def __init__(self, hidden_size=64):
+    def __init__(self, hidden_size=64, num_layers=1, dropout=0.0):
         super().__init__()
-        self.lstm = nn.LSTM(input_size=1, hidden_size=hidden_size, batch_first=True)
+        self.lstm = nn.LSTM(input_size=1, hidden_size=hidden_size,
+                            num_layers=num_layers, dropout=dropout,
+                            batch_first=True)
         self.fc = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
